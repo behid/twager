b79f19062ee79ba45bfdf093df0a3b5ad5498a61
Felix Aronsson <felix@mrfriday.com>
2012-11-07 23:30:12 +0100

    PoC implementation using flask

---
+++ twager.py
@@ -0,0 +1,26 @@
+#/usr/bin/env python
+from pagerduty import Pagerduty
+from flask import Flask
+import twilio.twiml
+
+app = Flask(__name__)
+
+@app.route('/twilio', methods=['GET', 'POST'])
+def twilio_resp():
+    pduty = Pagerduty('https://mrfriday.pagerduty.com/api/v1',
+                      '9FzxEwftrpRz3bSwyNa3')
+    oncall = pduty.find_oncall('PUIIRST')['entries'][0]['user']
+    oncall_contact = pduty.find_user_contact(oncall['id'])['contact_method']
+    print oncall_contact
+
+
+    phone = '+%s %s' % (oncall_contact['country_code'],
+                       ''.join(map(lambda x: '%s ' % x, oncall_contact['phone_number'])))
+    resp = twilio.twiml.Response()
+    resp.say('On call is %s, number %s' % (oncall['name'], phone))
+    return str(resp)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0')
+    
