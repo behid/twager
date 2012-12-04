43a1cce90ae430e6d8dd0cef84b0c501f3270f37
Felix Aronsson <felix@mrfriday.com>
2012-12-04 23:44:00 +0100

    Add method for SMS notification when going on call
    Adding a route that checks if there's a new OnCall person listed in
    pagerduty and, if so, send the new person a text message letting him
    know he's now the oncall.

--- twager.py
+++ twager.py
@@ -2,6 +2,7 @@
 from pagerduty import Pagerduty
 from flask import Flask
 import twilio.twiml
+from twilio.rest import TwilioRestClient
 
 import settings
 
@@ -18,6 +19,38 @@ def twilio_resp():
     resp = twilio.twiml.Response()
     resp.dial(phone)
     return str(resp)
+
+@app.route('/oncall-notif', methods=['GET'])
+def oncall_notification():
+    twilio = TwilioRestClient(settings.TWILIO_ACCOUNT, settings.TWILIO_TOKEN)
+    pduty = Pagerduty(settings.PAGERDUTY_API_URL, settings.PAGERDUTY_API_KEY)
+    oncall = pduty.find_oncall(settings.PAGERDUTY_SCHEDULE_ID)['entries'][0]['user']
+
+    new_oncall = True
+
+    with open(settings.TMPFILE, 'r+') as f:
+        try:
+            if f.read() == oncall['id']:
+                new_oncall = False
+            else:
+                f.seek(0)
+                f.write(oncall['id'])
+                f.truncate()
+        except IOError:
+            new_oncall = False
+            f.write(oncall['id'])
+
+    if new_oncall:
+        oncall_contact = pduty.find_user_contact(oncall['id'])['contact_method']
+        message = 'Hello %s, you are now OnCall.' % oncall['name']
+        phone = '+%s%s' % (oncall_contact['country_code'],
+                           oncall_contact['phone_number'])
+        twilio.sms.messages.create(to=phone, 
+                                   from_=settings.TWILIO_NUMBER,
+                                   body=message)
+        return "new oncall, sent sms"
+    else:
+        return "same oncall"
 
 if __name__ == '__main__':
     app.debug = settings.DEBUG
